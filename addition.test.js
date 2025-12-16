f0053b9f840a7b4b1b51bc095c16d214168118f1
nemuo14 <theo.merville@gmail.com>
2025-12-16 14:19:41 +0100

    without discribe

--- addition.test.js
+++ addition.test.js
@@ -1,5 +1,9 @@
 const { calculerAddition } = require('./addition');
-describe('calculerAddition', () => {
-    test('should return 20 when adding 15 and 5', () => {
+// describe('calculerAddition', () => {
+//     test('should return 20 when adding 15 and 5', () => {
+//         expect(calculerAddition(15, 5)).toBe(20);
+//     })})
+
+test('calculerAddition', () => {
         expect(calculerAddition(15, 5)).toBe(20);
-    })})
+    })
